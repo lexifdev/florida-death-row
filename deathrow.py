6973b6ea57c9f9dbfe905d19cfbc7e81e7a49ba4
lexifdev <sl@lxf.kr>
2017-06-05 10:08:13 +0900

    simpler code for inmate page.

--- deathrow.py
+++ deathrow.py
@@ -8,32 +8,22 @@ application = app
 
 # get the information for each ID
 def get_inmatedata(source, id):
-    for row in source:
-        if id == str( row["id"] ):
-            # decode handles accented characters
-            name = row["name"] 
-            race = row["race"]
-            photo = row["photo"] 
-            sex = row["sex"]
-            dob = row["dob"]
-            entry = row["entry"]
-            facility = row["facility"]
-            custody = row["custody"]
-    return id, name, race, photo, sex, dob, entry, facility, custody
+    return next((data for data in ROSTER if data['id'] == id))
 
-#set the homepage and the /restaurants.html to run the function to get information using the python dictionary INSPECTIONS
+
+# set the homepage and the /restaurants.html to run the function to get information using the python dictionary INSPECTIONS
 @app.route('/')
 @app.route('/inmates.html')
 def inmates():
     return render_template('inmates.html', roster=ROSTER)
 
 
-
-#make a path for the /restaurant with each restaurant id and show the information for each restaurant with each ID
+# make a path for the /restaurant with each restaurant id and show the information for each restaurant with each ID
 @app.route('/inmate/<id>.html')
 def inmate(id):
-    id, name, race, photo, sex, dob, entry, facility, custody = get_inmatedata(ROSTER, id)
-    return render_template('inmate.html', pairs=name, name=name, race=race, photo=photo, sex=sex, dob=dob, entry=entry, facility=facility, custody=custody)
+    inmatedata = get_inmatedata(ROSTER, id)
+    return render_template('inmate.html', inmate=inmatedata)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
